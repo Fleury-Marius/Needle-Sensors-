822bf4a9cffd6089cbed788688db448a088c5106
Fleury-Marius <mariorodriguesfleuryrosa@gmail.com>
2026-07-14 09:20:23 -0400

    Update VL61980X.ino

--- VL61980X.ino
+++ VL61980X.ino
@@ -1,5 +1,6 @@
 // Makerguides. (n.d.). VL6180X distance sensor with Arduino from 
 //https://www.makerguides.com/vl6180x-distance-sensor-with-arduino/
+//Designed for running the distance sensor VL6180X
 #include "Wire.h"
 #include "VL6180X.h"
 
